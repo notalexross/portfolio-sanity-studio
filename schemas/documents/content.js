d4c73047d21ce0a824c47feb107b4372dbc0d19f
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-04-15 11:16:44 +0100

    feat: prevent create/delete actions

--- schemas/documents/content.js
+++ schemas/documents/content.js
@@ -2,7 +2,7 @@ export default {
   name: 'content',
   title: 'Content',
   type: 'document',
-  // __experimental_actions: ['update', /* 'create', 'delete', */ 'publish'],
+  __experimental_actions: ['update', /* 'create', 'delete', */ 'publish'],
   preview: {
     prepare() {
       return {
