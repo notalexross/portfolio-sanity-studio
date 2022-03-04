9c3877d2612678e5626f79b3b5a4d0bd6889f41c
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2022-03-04 19:45:49 +0000

    feat: remove unnecessary dashboard widgets

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -5,29 +5,7 @@ export default {
       name: 'project-info'
     },
     {
-      name: 'netlify',
-      options: {
-        sites: [
-          {
-            title: 'Portfolio Live',
-            apiId: process.env.SANITY_STUDIO_NETLIFY_APP_ID,
-            buildHookId: process.env.SANITY_STUDIO_NETLIFY_BUILD_HOOK_ID,
-            name: process.env.SANITY_STUDIO_NETLIFY_SITE_NAME
-          },
-          {
-            title: 'Studio Live',
-            apiId: process.env.SANITY_STUDIO_NETLIFY_STUDIO_APP_ID,
-            buildHookId: process.env.SANITY_STUDIO_NETLIFY_STUDIO_BUILD_HOOK_ID,
-            name: process.env.SANITY_STUDIO_NETLIFY_STUDIO_SITE_NAME
-          }
-        ]
-      }
-    },
-    {
       name: 'project-users'
-    },
-    {
-      name: 'sanity-tutorials'
     }
   ]
 }
